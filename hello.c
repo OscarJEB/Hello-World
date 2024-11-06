99ac33c737819944aeb45438c41cd112bcd040b2
OscarJEB <burgeo1@macstudent.com.au>
2024-11-06 11:12:35 +1100

    Followed the cs50 video. Permissions was denied for agree and loops.

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+#include <cs50.h>
 int main(void)
 {
-    printf("What are you doing, Dave\n");
+    string answer = get_string("What's your name?");
+    printf("Hello, %s\n", answer);
 }
+
+//to compile when make hello.c doesn't work: cc hello.c -o hello cs50.c
